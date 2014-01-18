8f46f77592b0679f4fc960f2f6d0dd92416f9fdc
Timothy Michael <timothyjmichael@gmail.com>
2014-01-17 23:57:53 -0600

    --Initial pass at script.

--- getReplicaInformation.py
+++ getReplicaInformation.py
@@ -8,23 +8,69 @@
 # Copyright:   (c) tmichael 2014
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-#!/usr/bin/env python
-
 import arcpy
+import os
+import smtplib
 import sys
 import traceback
 
 
+#Script Variables
+connFileDirectory = r'C:\TEMP\ConnectionFiles'
+arcpy.env.workspace = connFileDirectory
+
+#Email Settings
+emailBody = ''
+emailSubject = 'Replica Information'
+emailRecipient = ''
+smtpuser = ''
+smtppass = ''
 
 
+#Script Functions
+def getGDBName(gdb):
+    fileName = os.path.basename(gdb)
+    dbName = os.path.splitext(fileName)[0]
+    return dbName
 
-arcpy.env.workspace = "C:/Data/myData.gdb"
 try:
-    ''' Code Here '''
+    #Get a list of SDE files in the specified directory
+    workspaces = arcpy.ListWorkspaces('*', 'SDE')
+
+    #Get the replicas in each dataabse
+    for gdb in workspaces:
+        #Get the geodatabase name from the file name
+        #Add geodatabase name to email body
+        gdbName = getGDBName(gdb)
+        emailBody += gdbName + '\n'
+
+
+        for replica in arcpy.da.ListReplicas(gdb):
+            #Add replica name to email body
+            emailBody += '\t' + replica.name + '\n'
+
+            #Determine the replica role (Parent / Child)
+            #Print when last changes sent or received based on role type
+            replicaRole = replica.isParent
+            if replicaRole == True:
+                emailBody += '\t\t Last Changes Sent' + ' ' + str(replica.lastSend) + '\n'
+            else:
+                emailBody += '\t\t Last Changes Received' + ' ' + str(replica.lastReceive) + '\n'
 
 
 
-    ''' Handle Errors '''
+
+    #Finally
+    print emailBody
+    #Send email
+    #server = smtplib.SMTP('smtp.gmail.com', 587)
+    #server.ehlo()
+    #erver.starttls()
+    #server.ehlo()
+    #server.login(smtpuser, smtppass)
+    #server.sendmail(smtpuser, emailRecipient, emailBody)
+    #server.quit()
+
 except arcpy.ExecuteError:
     # Get the tool error messages
     msgs = arcpy.GetMessages(2)
