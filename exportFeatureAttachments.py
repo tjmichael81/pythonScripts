e1654e2a40f044e2506e3bf6b91465633619f10a
Timothy Michael <timothyjmichael@gmail.com>
2015-11-24 16:48:49 -0500

    Recently updated the script to also add the paths to the exported feature attachments to the source feature class table.  This will allow the paths to work correctly in ArcGIS and other places

--- exportFeatureAttachments.py
+++ exportFeatureAttachments.py
@@ -4,9 +4,11 @@ import arcpy
 import os
 
 #Script Variables
+sourceFeatureTable = r'C:\TEMP\Database.gdb\AttachmentTable__ATTACH'
 sourceAttachmentTable = r'C:\TEMP\Database.gdb\AttachmentTable__ATTACH'
 targetOutputFolder = r'C:\TEMP\AttachmentExport'
 filePathAttribute = 'FILE_PATH'
+localPathExpression = '!AttachmentTable__ATTACH.FILE_PATH!'
 
 #Create a new column to contain the path to each exported file
 if not filePathAttribute in arcpy.ListFields(sourceAttachmentTable):
@@ -29,6 +31,19 @@ with arcpy.da.SearchCursor(sourceAttachmentTable, [filePathAttribute, 'DATA']) a
         del row
 del cursor
 
+#Create a new column to contain the path to each exported file
+if not filePathAttribute in arcpy.ListFields(sourceFeatureTable):
+    arcpy.AddField_management(sourceAttachmentTable, filePathAttribute, 'TEXT', '', '', 200)
+
+#Make feature layer & table view to prepare for join
+arcpy.MakeFeatureLayer_management(sourceFeatureTable, 'featureTable')
+arcpy.MakeTableView_management(sourceAttachmentTable, 'tableView')
+
+# Add a join between the feature table and table view
+arcpy.AddJoin_management('featureTable', 'OBJECTID', 'tableView', 'REL_OBJECTID')
+
+# Calculate the value for the file path URL to the feature table
+arcpy.CalculateField_management('featureTable', 'AttachmentTable.FILE_PATH', localPathExpression, 'PYTHON')
 
 if __name__ == '__main__':
     main()
