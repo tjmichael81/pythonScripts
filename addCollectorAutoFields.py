810d097072f407e76eddc72d314fff6a3f93f4a6
tjmichael81 <timothyjmichael@gmail.com>
2016-10-21 22:31:41 -0400

    Added error handling boilerplate from Esri
    http://pro.arcgis.com/en/pro-app/arcpy/get-started/error-handling-with-python.htm

--- addCollectorAutoFields.py
+++ addCollectorAutoFields.py
@@ -17,11 +17,43 @@ def main():
 ################################################################################
 import arcpy
 import datetime
+import sys
+import traceback
 
 ################################################################################
 # Script Functions
 ################################################################################
 
+
+try:
+    print("In try block")
+
+except arcpy.ExecuteError:
+    # Get the tool error messages
+    msgs = arcpy.GetMessages(2)
+
+    # Return tool error messages for use with a script tool
+    arcpy.AddError(msgs)
+
+    # Print tool error messages for use in Python/PythonWin
+    print(msgs)
+
+except:
+    # Get the traceback object
+    tb = sys.exc_info()[2]
+    tbinfo = traceback.format_tb(tb)[0]
+
+    # Concatenate information together concerning the error into a message string
+    pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info()[1])
+    msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+
+    # Return python error messages for use in script tool or Python Window
+    arcpy.AddError(pymsg)
+    arcpy.AddError(msgs)
+
+    # Print Python error messages for use in Python / Python Window
+    print(pymsg)
+    print(msgs)
 
 if __name__ == '__main__':
     main()
