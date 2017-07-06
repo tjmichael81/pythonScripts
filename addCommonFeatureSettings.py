b1f737548c8921136a5a152deaa64212de563c39
tjmichael81 <timothyjmichael@gmail.com>
2017-07-06 08:32:40 -0400

    Begin new script to add commonly used fields / features to all feature classes in a geodatabase

---
+++ addCommonFeatureSettings.py
@@ -0,0 +1,7 @@
+# -------------------------------------------------------------------------------
+# DESCRIPTION #
+# Add commonly used settings to all feature classes in a geodatabase:
+# - Editor Tracking
+# - GlobalID
+# - Feature Attachments
+# -------------------------------------------------------------------------------
